e75cc0c50d43f9dce45b4008cb248cdde4a93acb
Ilya Dogolazky <ilya.dogolazky@nokia.com>
2010-12-27 12:36:12 +0200

    copyright header added

--- src/server/state.h
+++ src/server/state.h
@@ -1,3 +1,26 @@
+/***************************************************************************
+**                                                                        **
+**   Copyright (C) 2009-2010 Nokia Corporation.                           **
+**                                                                        **
+**   Author: Ilya Dogolazky <ilya.dogolazky@nokia.com>                    **
+**   Author: Simo Piiroinen <simo.piiroinen@nokia.com>                    **
+**   Author: Victor Portnov <ext-victor.portnov@nokia.com>                **
+**                                                                        **
+**     This file is part of Timed                                         **
+**                                                                        **
+**     Timed is free software; you can redistribute it and/or modify      **
+**     it under the terms of the GNU Lesser General Public License        **
+**     version 2.1 as published by the Free Software Foundation.          **
+**                                                                        **
+**     Timed is distributed in the hope that it will be useful, but       **
+**     WITHOUT ANY WARRANTY;  without even the implied warranty  of       **
+**     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               **
+**     See the GNU Lesser General Public License  for more details.       **
+**                                                                        **
+**   You should have received a copy of the GNU  Lesser General Public    **
+**   License along with Timed. If not, see http://www.gnu.org/licenses/   **
+**                                                                        **
+***************************************************************************/
 #ifndef STATE_H
 #define STATE_H
 
@@ -337,4 +360,3 @@ struct state_dlg_resp_t : public abstract_state_t
 
 
 #endif//STATE_H
-
