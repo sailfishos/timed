d7365ea62866db52bbcf5bdf4d39f5c65a6aafc6
Ilya Dogolazky <ilya.dogolazky@nokia.com>
2011-02-14 20:20:49 +0200

    using new qmlog features for mountbale directory

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -75,7 +75,11 @@ int main(int ac, char **av)
 #if F_HOME_LOG
   bool log_file_at_home = access(F_FORCE_HOME_LOG_PATH, F_OK) == 0 ;
   if (log_file_at_home)
-    (new qmlog::log_file("/home/user/MyDocs/timed.log", varlog_level)) ->enable_fields(qmlog::Monotonic_Milli | qmlog::Time_Milli) ;
+  {
+    system("touch /home/user/MyDocs/timed.log") ;
+    qmlog::log_file *home = new qmlog::log_file("/home/user/MyDocs/timed.log", varlog_level) ;
+    home->enable_fields(qmlog::Monotonic_Milli | qmlog::Time_Milli | qmlog::Close_After_Write | qmlog::Cache_If_Cant_Open | qmlog::Dont_Create_File | qmlog::Retry_If_Failed) ;
+  }
 #endif
 
   bool isatty_2 = isatty(2) ;
