d8e20fbedf980d73b502ae471bf385e7c738e2bb
Ilya Dogolazky <ilya.dogolazky@nokia.com>
2011-04-04 12:40:53 +0300

    using typedef'ed matatype names in adaptor class

--- src/server/adaptor.h
+++ src/server/adaptor.h
@@ -163,19 +163,19 @@ public slots:
     timed->am->get_event_attributes(cookie_t(cookie), a) ;
   }
 
-  void get_attributes_by_cookie(uint cookie, const QDBusMessage &message, QMap<QString,QString> &a)
+  void get_attributes_by_cookie(uint cookie, const QDBusMessage &message, Q_Map_String_String &a)
   {
     log_notice("DBUS::com.nokia.time.get_attributes_by_cookie(%d) by %s", cookie, PEER) ;
     timed->am->get_attributes_by_cookie(cookie, a) ;
   }
 
-  void get_attributes_by_cookies(const QList<uint> &cookies, const QDBusMessage &message, QMap<uint, QMap<QString,QString> > &a)
+  void get_attributes_by_cookies(const Q_List_uint &cookies, const QDBusMessage &message, Q_Map_uint_String_String &a)
   {
     log_notice("DBUS::com.nokia.time.get_attributes_by_cookies[%d] by %s", cookies.size(), PEER) ;
     timed->am->get_attributes_by_cookies(cookies, a) ;
   }
 
-  void get_cookies_by_attributes(const QMap<QString,QString> &words, const QDBusMessage &message, QList<uint> &res)
+  void get_cookies_by_attributes(const Q_Map_String_String &words, const QDBusMessage &message, Q_List_uint &res)
   {
     log_notice("DBUS::com.nokia.time.get_cookies_by_attributes(...) by %s", PEER) ;
     timed->am->get_cookies_by_attributes(words, res) ;
@@ -187,7 +187,7 @@ public slots:
     return timed->cancel(cookie_t(cookie)) ;
   }
 
-  void cancel_events(const QList<uint> &cookies, const QDBusMessage &message, QList<uint> &failed)
+  void cancel_events(const Q_List_uint &cookies, const QDBusMessage &message, Q_List_uint &failed)
   {
     log_notice("DBUS::com.nokia.time.cancel_events([%d]) by %s", cookies.size(), PEER) ;
     timed->cancel_events(cookies, failed) ;
